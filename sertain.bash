d520d9d62d45313488962974b7776af5cf1db5b8
Somajit <dey.somajit@gmail.com>
2021-05-09 11:38:58 +0530

    Fixed printf error

--- sertain.bash
+++ sertain.bash
@@ -3,27 +3,27 @@
 # No need for nodejs, go etc. Just pure bash scripting.
 
 httparse(){
-  # Description: http request header parser. Reads http request headers. Output is json.
+  # Description: http request header parser.
+  # I/O: Reads http request headers from stdin. Output at stdout is json.
   # Parameters: none
   # TODO: parse query strings from path
 
   local method path version
   read -rd $'\r\n' method path version && read
 
-  echo "{"
+  echo "{" # Open json
 
-  printf "\"method\": \"${method//\"/\\\"}\"" 
-  printf ",\n\"path\": \"${path//\"/\\\"}\"" 
-  printf ",\n\"version\": \"${version//\"/\\\"}\"" 
+  printf "%s" "\"method\": \"${method//\"/\\\"}\""
+  printf ",\n%s" "\"path\": \"${path//\"/\\\"}\""
+  printf ",\n%s" "\"version\": \"${version//\"/\\\"}\""
 
   local header value
   while IFS=: read -rd $'\r\n' header value && read && [[ -n "${header}" ]];do
     value="${value//\"/\\\"}"
-    echo ","
-    echo -n "\"${header}\": \"${value#' '}\""
+    printf ",\n%s" "\"${header}\": \"${value#' '}\""
   done
 
-  echo -e "\n}"
+  printf "\n}\n" # Close json
 
   return 0
   ### Next read should be from payload
@@ -31,21 +31,23 @@ httparse(){
 
 status(){
   # Description: Print the response status-line for v1.1 based on given status-code and reason-phrase.
+  # I/O: No input. Output at stdout.
   # Parameters: $1=status-code $2=reason-phrase
   # TODO:
   
-  printf "HTTP/1.1 ${1} ${2}\r\n"
+  printf "%s\r\n" "HTTP/1.1 ${1} ${2}"
   
   return 0
 }; export -f status
 
 header(){
   # Description: Print header with given key and value.
+  # I/O: No input. Output at stdout.
   # Parameters: $1=header-key $2=header-value
   # TODO:
   
   if [[ -n "${1}" ]]; then
-    printf "${1}: ${2}\r\n"
+    printf "%s\r\n" "${1}: ${2}"
   else
     printf "\r\n"
   fi
@@ -53,22 +55,23 @@ header(){
   return 0
 }; export -f header
 
-
 dl_payload(){
   # Description: Read and output client-sent payload.
+  # I/O: Read from stdin. Write to stdout.
   # Parameters: Content-Length as obtained from: httparse | jq -r '."Content-Length"'
   # TODO:
 
   if (("${1}"==0));then return; fi # No content
   local payload
   read -N "${1}" payload
-  printf "${payload}"
+  echo "${payload}"
   
   return 0
 }; export -f dl_payload
 
 dl_stream(){
   # Description: Read and output client-sent stream. Decide if it is stream using Transfer-Encoding header
+  # I/O: stdin/stdout.
   # Parameters: None
   # TODO:
 
@@ -78,7 +81,7 @@ dl_stream(){
     if ((size==0)); then read && break; fi # Check if end of stream
     local chunk
     read -d $'\r\n' -N "$((16#${size}))" chunk && read # Convert chunk-size to decimal from hex
-    printf "${chunk}"
+    printf "%s" "${chunk}" #Or, use "echo -n" instead of 'printf "%s"'
   done
   
   return 0
@@ -86,6 +89,7 @@ dl_stream(){
 
 ul_payload(){
   # Description: Write payload to be uploaded and the corresponding headers at stdout.
+  # I/O: Read from stdin or file. Write to stdout.
   # Parameters: Filename to read payload from. Otherwise read payload from stdin.
   # TODO:
 
@@ -104,6 +108,7 @@ ul_payload(){
 
 ul_stream(){
   # Description: Write stream to be uploaded and the corresponding headers at stdout.
+  # I/O: stdin/stdout.
   # Parameters: None
   # Remarks: This function runs an infinite loop. To denote end of stream, send SIGTERM.
   # pid required to send SIGTERM is output at stderr
@@ -129,7 +134,7 @@ ul_stream(){
     done
     if ((size!=0));then
       printf "%x\r\n" "${size}"
-      printf "${chunk}\r\n"
+      printf "%s\r\n" "${chunk}"
     fi
   done
   printf "0\r\n\r\n" # End of stream marker
